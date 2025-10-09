3d26d3ac59cd31d40eaeb10ad811c1ad51a57b8b
praveen-7050 <praveen.n7050@gmail.com>
2025-10-09 19:58:32 +0530

    making reponsive js projects

--- Digital Timer/Digital Timer.js
+++ Digital Timer/Digital Timer.js
@@ -1,22 +1,29 @@
-const hourtime=document.getElementById("tim-hr")
+const hourtime = document.getElementById("tim-hr");
 const minutetime = document.getElementById("tim-min");
 const secondtime = document.getElementById("tim-sec");
 
-function digitaltimer(){
-    let hours=new Date().getHours();
-    let minutes=new Date().getMinutes();
-    let seconds=new Date().getSeconds();
-
+function digitaltimer() {
+  let hours = new Date().getHours();
+  let minutes = new Date().getMinutes();
+  let seconds = new Date().getSeconds();
+  let ampm = "AM";
+  if (hours >= 12) {
+    ampm = "PM";
+    if (hours > 12) hours -= 12;
+  }
+  if (hours === 0) {
+    hours = 12;
+  }
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-     
-    hourtime.innerHTML=hours;
-    minutetime.innerHTML=minutes;
-    secondtime.innerHTML=seconds;
 
-    setInterval(()=>{
-    digitaltimer();
-    },500)
-};
+  hourtime.innerHTML = hours;
+  minutetime.innerHTML = minutes;
+  secondtime.innerHTML = seconds;
+
+  document.getElementById("tim-hourr").innerText = ampm;
+  setTimeout(digitaltimer, 1000);
+}
+
 digitaltimer();
--- Random Password generator/index.js
+++ Random Password generator/index.js
@@ -1,20 +1,31 @@
-function randompasswordgeneator(){
-    let characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$&?#%";
-    let len=10;
-    let password="";
+function randompasswordgenerator() {
+  let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$&?#%";
+  let len = 10;
+  let password = "";
 
-    for(let i = 0;i < len;i++){
-        let randomchar=Math.floor(Math.random()*characters.length);
-        password +=characters[randomchar];
-    }
-   document.getElementById("password").value=password
+  for (let i = 0; i < len; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
 
+  document.getElementById("password").value = password;
 }
 
-function copyclipboard(){
-    let copy=document.getElementById("password").value
-    navigator.clipboard.writeText(copy);
-    alert("password is copied "+ password.value )
-       document.getElementById("password").value=""
-}
+function copyclipboard() {
+  let passwordField = document.getElementById("password");
+  let copy = passwordField.value;
 
+  if (copy === "") {
+    alert("Please generate a password first!");
+    return;
+  }
+
+  navigator.clipboard
+    .writeText(copy)
+    .then(() => {
+      alert("Password copied: " + copy);
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
+}
